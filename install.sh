378915c1760c9f516f28017df343d1ad4754e683
Dominik Cencek <me@dominikcencek.com>
2014-11-08 15:27:37 +0100

    Remove linux support

--- install.sh
+++ install.sh
@@ -13,10 +13,7 @@ prompt_() {
 }
 
 colorize() { echo -e "\x1B[1;$2m$1\x1B[0m"; }
-install_apt() { if hash apt-get 2>/dev/null; then sudo apt-get install $1; fi }
 install_osx() { if hash brew 2>/dev/null; then brew install $1; fi }
-install_yum() { if hash yum 2>/dev/null; then sudo yum install $1; fi }
-install_arch() { if hash pacman 2>/dev/null; then sudo pacman -S $1; fi }
 
 try_unlink() {
   if [ -f $1 ] && [ ! -L $1 ] || [ -d $1 ] && [ ! -L $1 ]; then
@@ -38,10 +35,7 @@ link_() {
 }
 
 if prompt_ "Install suggested packages? (recommended)"; then
-  install_apt "git zsh tmux silversearcher-ag tig curl tree"
-  install_arch "git zsh tmux tig the_silver_searcher tree"
-  install_yum "git vim zsh tmux the_silver_searcher tig tree"
-  install_osx "tmux"
+  install_osx "tmux rbenv ruby-build rbenv-gem-rehash"
 fi;
 
 hash git 2>/dev/null || { echo >&2 "Git not found. Aborting"; exit 1; }
@@ -52,32 +46,8 @@ hash curl 2>/dev/null || { echo >&2 "curl not found. Aborting"; exit 1; }
 colorize "Installing dominikc/dotfiles" 32
 
 if prompt_ "Install ruby (rbenv)?"; then
-  if prompt_ "Install rbenv?"; then
-    if try_unlink "$HOME/.rbenv"; then
-      colorize "Installing rbenv..." 32
-      git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
-    fi
-  fi
-
-  if prompt_ "Install ruby-build?"; then
-    colorize "Installing suggested packages" 32
-    install_osx "openssl libyaml"
-    install_apt "autoconf bison build-essential libssl-dev libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev"
-    install_yum "gcc-c++ glibc-headers glibc-devel openssl-devel readline libyaml-devel readline-devel zlib zlib-devel patch"
-
-    if prompt_ "Install pg/sqlite libs"; then
-      install_apt "libsqlite3-dev libpq-dev"
-      install_yum "libsqlite3x-devel postgresql-devel"
-      install_arch "postgresql-libs sqlite3"
-    fi
-
-    if try_unlink "$HOME/.rbenv/plugins/ruby-build"; then
-      git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
-    fi
-  fi
-
   if prompt_ "Install ruby-2.1.3?"; then
-    rbenv install 2.1.3 && rbenv global 2.1.3
+    rbenv install 2.1.4 && rbenv global 2.1.4
   fi
 fi
 
@@ -96,11 +66,11 @@ if prompt_ "Install oh-my-zsh?"; then
 fi
 
 if prompt_ "Install Powerline fonts?"; then
-  git clone git://github.com/Lokaltog/powerline-fonts.git $HOME/powerline
+  git clone git://github.com/Lokaltog/powerline-fonts.git /tmp/powerline
+  /tmp/powerline/install.sh
 fi
 
-
-files=(agignore editorconfig tmux.conf tmuxline vimrc zshrc)
+files=(agignore editorconfig tmux.conf tmuxline vimrc zshrc tigrc)
 
 for file in ${files[*]}
 do
@@ -115,25 +85,5 @@ if prompt_ "Install gitconfig?"; then
       $DOTFILES/gitconfig > $HOME/.gitconfig
   fi
 fi
-
-if prompt_ "Install extra dotfiles?"; then
-  mkdir -p $HOME/.ncmpcpp
-  DOTFILES="$ORIG_DOTFILES/extra"
-  files=(Xdefaults i3 tigrc ncmpcpp/config)
-  for file in ${files[*]}
-  do
-    if try_unlink "$HOME/.$file"; then (link_ $file); fi
-  done
-fi
-
-if hash pacman 2>/dev/null; then
-  DOTFILES="$ORIG_DOTFILES/archlinux"
-  files=(zshrc yaourtrc)
-  for file in ${files[*]}
-  do
-    if try_unlink "$HOME/.$file"; then (link_ $file); fi
-  done
-fi;
-
 
 echo "Type 'vundle install' to install VIM plugins"
